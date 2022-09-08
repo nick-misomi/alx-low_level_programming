e3b67463be6849297667ab4f39e562cff016379b
nick-misomi <mwikasomi@gmail.com>
2022-09-08 15:56:27 -0700

    hot fix on task 4 and 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * return: 0 after printing function
  */
 int main (void)
-{puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle,");
 	return(0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,6 @@
  * return: always 0 (success)
  */
 int main (void)
-{printf ("with proper grammar, but the outcome is a piece of art");
+{printf ("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
