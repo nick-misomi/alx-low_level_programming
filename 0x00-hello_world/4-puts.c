46404a04c0168816531f46d11b9de8dbbfc9658e
nick-misomi <mwikasomi@gmail.com>
2022-09-08 16:18:19 -0700

    hot fix on task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
  * return: 0 after printing function
  */
 int main (void)
-{puts("\"Programming is like building a multilingual puzzle,");
+{
+	puts("\"Programming is like building a multilingual puzzle,");
 	return(0);
 }
