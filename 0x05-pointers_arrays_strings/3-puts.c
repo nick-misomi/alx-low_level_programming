6fbc235571432eebf431797244e692443cfea62e
nick-misomi <mwikasomi@gmail.com>
2022-09-19 11:54:24 -0700

    Hot fix task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,16 +2,18 @@
 #include "main.h"
 
 /**
-*_puts - prints a string
-*@str: string to print
-*
-*description: print a string
-*on success: return no error
-*/
+ *_puts - prints a string
+ *@str: string to print
+ *
+ *description: print a string
+ *on success: return no error
+ */
+
 void _puts(char *str)
 {
 	int i = 0;
-	while (*(str + i) != '\0');
+
+	while (*(str + i) != '\0')
 	{
 		putchar (*(str + i));
 		i++;
