82986527d357a84a2ffa234d46263aad28011d73
nick-misomi <mwikasomi@gmail.com>
2022-09-19 12:02:55 -0700

    Hot fix task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
-*strlen - returns the length of a string
-*@s: string
-*return: length as int;
-*/
+ *strlen - returns the length of a string
+ *@s: string
+ *return: length as int;
+ */
+
 int _strlen(char *s)
 {
 	int len = 0;
-	
-	while(*(s + len) != '\0')
+		
+	while (*(s + len) != '\0')
 		len++;
+
 	return (len);
 }
