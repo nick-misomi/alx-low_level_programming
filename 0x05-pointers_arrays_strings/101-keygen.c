b15a0dd08250d972fec617bc9fe2c894d179faa5
nick-misomi <mwikasomi@gmail.com>
2022-09-20 13:41:13 -0700

    solution on advanced tasks 10 and 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+*main - entry point
+*Returns: generated password
+*/
+
+int main(void)
+{
+	char c;
+	int y;
+
+	srand(time(0));
+	while (y <= 2645)
+	{
+		c = rand() % 128;
+		y += c;
+		putchar (c);
+	}
+	putchar (2772 - x);
+
+	return (0);
+}
