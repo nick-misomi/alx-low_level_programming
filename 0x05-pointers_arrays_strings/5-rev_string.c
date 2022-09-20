b2a009fbc9ef2f49307e2e18fa7a873c595f5c59
nick-misomi <mwikasomi@gmail.com>
2022-09-20 12:46:31 -0700

    Hot fix on task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,48 +1,35 @@
 #include <stdio.h>
-#include <string.h>
 #include "main.h"
 
+/**
+* rev_string - Reverses a string
+*@s: string to be reversed
+*return: nothing
+*/
 
-int str_len(char *s);
-void rev_string(char *s);
-int main(void)
-{
-	char s[50] = "My School";
-
-	printf("String to be reversed is: %s", s);
-
-	revstr(s);
-
-	printf("The reverse string is: %s", s);
-	return (0);
-}
 void rev_string(char *s)
 {
-	int len, i;
-	char *start, *end, tmp;
-
-	len = str_len(s);
-	start = s;
-	end = s;
-
-	for (i = 0; i < len - 1; i++)
-	end++;
+	int i, tmp, len = _strlen(s);
 
 	for (i = 0; i < len / 2; i++)
 	{
-		tmp = *end;
-		*end = *start;
-		*start = tmp;
-
-		start++;
-		end__;
+		tmp = *(s + i);
+		*(s + i) = (s + len - i - 1);
+		*(s + len - i - 1) = tmp;
 	}
 }
-int str_len(char *ptr)
-{
-	int i = 0;
+/**
+ * _strlen - returns the length of a string
+ *@s: string
+ *return: the length of the string
+ */
 
-	while (*(ptr + i) != '\0')
-	i++;
-	return (i);
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
 }
