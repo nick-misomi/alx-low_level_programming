fd551be7133323c2eb5856f3d8fb1d277fe61313
nick-misomi <mwikasomi@gmail.com>
2022-09-22 05:14:39 -0700

    solution of task 0 to 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*rot13 - encodes a string into rot13
+*@s: string to encode
+*
+*return: address of s
+*/
+
+char *rot13(char *s)
+{
+	int i, j;
+	char x[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char y[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (x[j] == *(s + i))
+			{
+				*(s + i) = y[j];
+				break;
+			}
+		}
+	}
+	return (s)
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strncpuy - c function that copies a string
+*description: including the terminating null byte
+*if the length of the source string is less thsn the maximum byte number
+*the remainder of the destination string is filled with null bytes.
+*@dest: buffer storing the string copy
+*@src: the source string
+*@n: max number of byte copied
+*return: returns
+*/
+char *_strndcpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
