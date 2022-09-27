bae2b072148917956f85beec013eeed0f39693e3
nick-misomi <mwikasomi@gmail.com>
2022-09-27 02:10:49 -0700

    hot fix task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,7 +9,7 @@
 *return: pointer to the copied memory block
 */
 
-char *_memcpy(char *dest, char *src, insigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
