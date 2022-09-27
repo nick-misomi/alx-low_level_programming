2da2119a8e4586b66e63767cf443d35686fa7dcb
nick-misomi <mwikasomi@gmail.com>
2022-09-27 03:22:34 -0700

    hot fix old staff on pointer_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,8 +1,6 @@
 #include "main.h"
 
-char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
 /**
 *add_strings - adds the number stored in 2 strings.
 *@n1: the string containing the first number to be added.
@@ -18,13 +16,18 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int num, tens = 0;
 
-	for (; *n1 && *n2; n1--, n2--, r_index)
+	for (; *n1 && *n2; n1--; n2--; r_index)
 	{
 		num = (*n1 - '0') + (*n2 - '0');
 		num += tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
+	for (; *n1 n1-- r_index++)
+	{
+		num = *(n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
 
 	for (; *n2; n2--; r_index--)
 	{
@@ -32,4 +35,5 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
+
 }
