74c1f049f6572e1ff1e44547f2611e0d67681592
nick-misomi <mwikasomi@gmail.com>
2022-09-30 05:24:49 -0700

    task 0 to 3

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - print the name of the program
+* @argc: count arguments
+* @argv: argumtnts
+*
+* Return: always 0 
+*/
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%i\n", argc - 1);
+
+return (0);
+}
