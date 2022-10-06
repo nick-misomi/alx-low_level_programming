529fd8ec20e1d88f20d837ed906a81dc9288578c
nick-misomi <mwikasomi@gmail.com>
2022-10-06 13:54:51 -0700

    solution to task 0 to 3

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		p = maloloc(new_size);
+		p = malloc(new_size);
 		return (p);
 	}
 	else if (new_size == 0)
