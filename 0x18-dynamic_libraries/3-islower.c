d6e9da36f94fc79d4b61adb010f5d83a0c4881c8
nick-misomi <mwikasomi@gmail.com>
2022-12-16 12:03:07 -0800

    task 0, task 1

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - func to check for lowercase char
+ * @c: is thr int that will use for the argument of the funtion
+ * return:0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else 
+		return (0);
+}
